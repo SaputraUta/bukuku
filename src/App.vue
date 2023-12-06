3b87ca9a85a215c86ce88624fc14a870a6857852
Saputra <saputra.uta50@gmail.com>
2023-12-06 23:10:50 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import Greet from "./components/Books.vue";
+import { ref } from "vue";
+const showSideBar = ref(false);
+</script>
+
+<template>
+  <div class="flex min-h-screen">
+    <div v-if="showSideBar" class="w-64 bg-gray-200 border-r border-gray-200">
+      <div class="mt-16">
+        <a href="" class="flex gap-2 mb-8 hover:cursor-pointer hover:scale-105"
+          ><img src="./assets/house-solid.svg" alt="" class="ml-5" />
+          <p class="text-slate-800text-lg">Home</p></a
+        >
+        <a href="" class="flex gap-2 mb-8 hover:cursor-pointer hover:scale-105"
+          ><img src="./assets/phone-solid.svg" alt="" class="ml-5" />
+          <p class="text-slate-800 text-lg">Contact us</p></a
+        >
+        <a href="" class="flex gap-2 mb-8 hover:cursor-pointer hover:scale-105"
+          ><img src="./assets/address-book-solid.svg" alt="" class="ml-5" />
+          <p class="text-slate-800 text-lg">About us</p></a
+        >
+      </div>
+    </div>
+    <div class="flex-1">
+      <div
+        class="flex justify-between border-b-2 border-gray-200 h-20 items-center relative"
+      >
+        <button @click="showSideBar = !showSideBar">
+          <img src="./assets/bars-solid.svg" alt="" class="ml-5 w-8" />
+        </button>
+        <div class="flex hover:cursor-pointer items-center mx-auto">
+          <img src="./assets/book-solid.svg" alt="book" class="w-10 ml-5" />
+          <h2 class="font-bold text-4xl ml-2 text-slate-800">Bukuku</h2>
+        </div>
+        <div class="mr-16 flex gap-4 items-center">
+          <h2 class="text-2xl font-bold text-slate-800">Hi, Saputra!</h2>
+          <img
+            src="./assets/IMG_9359.jpg"
+            alt=""
+            class="object-cover w-14 h-14 rounded-full border-2 border-slate-800 hover:cursor-pointer"
+          />
+        </div>
+      </div>
+      <div class="ml-5 mt-5 mr-5">
+        <h2 class="text-3xl font-bold text-slate-800">List of books</h2>
+        <Greet class="mt-5" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Books.vue
@@ -0,0 +1,85 @@
+<template>
+  <table class="w-full bg-white border border-gray-300">
+    <thead class="bg-gray-100">
+      <tr>
+        <th class="py-2 px-4 border text-slate-800 text-left">Nama Buku</th>
+        <th class="py-2 px-4 border text-slate-800 text-left">Pengarang Buku</th>
+        <th class="py-2 px-4 border text-slate-800 text-left">Tahun Rilis</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">Harry Potter</td>
+        <td class="py-2 px-4 border text-slate-800">J.K. Rowling</td>
+        <td class="py-2 px-4 border text-slate-800">1997</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">To Kill a Mockingbird</td>
+        <td class="py-2 px-4 border text-slate-800">Harper Lee</td>
+        <td class="py-2 px-4 border text-slate-800">1960</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Great Gatsby</td>
+        <td class="py-2 px-4 border text-slate-800">F. Scott Fitzgerald</td>
+        <td class="py-2 px-4 border text-slate-800">1925</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Catcher in the Rye,</td>
+        <td class="py-2 px-4 border text-slate-800">J.D. Salinger</td>
+        <td class="py-2 px-4 border text-slate-800">1951</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">Pride and Prejudice</td>
+        <td class="py-2 px-4 border text-slate-800">Jane Austen,</td>
+        <td class="py-2 px-4 border text-slate-800">1813</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Hobbit</td>
+        <td class="py-2 px-4 border text-slate-800">J.R.R. Tolkien</td>
+        <td class="py-2 px-4 border text-slate-800">1937</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Da Vinci Code</td>
+        <td class="py-2 px-4 border text-slate-800">Dan Brown</td>
+        <td class="py-2 px-4 border text-slate-800">2003</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Shining</td>
+        <td class="py-2 px-4 border text-slate-800">Stephen King</td>
+        <td class="py-2 px-4 border text-slate-800">1977</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Lord of the Rings</td>
+        <td class="py-2 px-4 border text-slate-800">J.R.R. Tolkien</td>
+        <td class="py-2 px-4 border text-slate-800">1955</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">Brave New World</td>
+        <td class="py-2 px-4 border text-slate-800">Aldous Huxley</td>
+        <td class="py-2 px-4 border text-slate-800">1932</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Alchemist</td>
+        <td class="py-2 px-4 border text-slate-800">Paulo Coelho</td>
+        <td class="py-2 px-4 border text-slate-800">1988</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">Gone with the Wind</td>
+        <td class="py-2 px-4 border text-slate-800">Margaret Mitchell</td>
+        <td class="py-2 px-4 border text-slate-800">1936</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border text-slate-800">The Hunger Games</td>
+        <td class="py-2 px-4 border text-slate-800">Suzanne Collins</td>
+        <td class="py-2 px-4 border text-slate-800">2008</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Greet",
+});
+</script>
